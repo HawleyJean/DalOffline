0fd74ce95d819622fd71fde3f6aa6974579b9c2c
Falahan <amuise3@hotmail.ca>
2017-03-14 14:47:40 -0300

    Fixed JUnit tests for authenticator

--- src/test/java/com/csci3130/daloffline/tests/AuthenticatorTest.java
+++ src/test/java/com/csci3130/daloffline/tests/AuthenticatorTest.java
@@ -2,9 +2,11 @@ package com.csci3130.daloffline.tests;
 
 import static org.junit.Assert.*;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.Test;
 
-import com.csci3130.daloffline.DalOfflineUI;
 import com.csci3130.daloffline.authentication.*;
 import com.csci3130.daloffline.initialization.DatabaseInitializer;
 
@@ -13,19 +15,15 @@ public class AuthenticatorTest {
 	@Test
 	public void authenticateTest() {
 		
-		String persistenceUnitName = "daloffline_db_testing";
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("daloffline_db");
 		
-	    DatabaseInitializer.generateUsers(persistenceUnitName);
+	    DatabaseInitializer.generateUsers(factory);
 		
-		assertEquals(true, Authenticator.authenticate("user", "pass"));
-		
-		assertNotEquals(true, Authenticator.authenticate("wrong", "pass"));
-		
-		assertNotEquals(true, Authenticator.authenticate("user", "passssssssssssssss"));
-		
-		assertNotEquals(true, Authenticator.authenticate("", ""));
-		
-		DatabaseInitializer.clearUsers(persistenceUnitName);
+		assertEquals(true, Authenticator.authenticate("user", "pass", factory));
+		assertNotEquals(true, Authenticator.authenticate("wrong", "pass", factory));
+		assertNotEquals(true, Authenticator.authenticate("user", "passssssssssssssss", factory));
+		assertNotEquals(true, Authenticator.authenticate("", "", factory));
+
 		
 	}
 
