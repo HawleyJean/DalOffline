656a911b3b1587a937fbe6a525945b8c855b8e5a
Connor Foran <connor@4n.ca>
2017-04-01 11:12:38 -0300

    Trying something with heroku

--- src/main/java/com/csci3130/daloffline/DalOfflineUI.java
+++ src/main/java/com/csci3130/daloffline/DalOfflineUI.java
@@ -2,6 +2,9 @@ package com.csci3130.daloffline;
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -60,8 +63,33 @@ public class DalOfflineUI extends UI {
 	 */
     @Override
     protected void init(VaadinRequest request) {
+    	String databaseUrl = System.getenv("DATABASE_URL");
+    	if(databaseUrl != null)
+    	{
+	    	StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+	    	String dbVendor = st.nextToken(); //if DATABASE_URL is set
+	    	String userName = st.nextToken();
+	    	String password = st.nextToken();
+	    	String host = st.nextToken();
+	    	String port = st.nextToken();
+	    	String databaseName = st.nextToken();
+	    	String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
+	    	Map<String, String> properties = new HashMap<String, String>();
+	    	properties.put("javax.persistence.jdbc.url", databaseUrl );
+	    	properties.put("javax.persistence.jdbc.user", userName );
+	    	properties.put("javax.persistence.jdbc.password", password );
+	    	properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+	    	properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+	    	factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
+    	}
+    	else
+    		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
     	
-    	factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+    	//Map<String, Object> configOverrides = new HashMap<String, Object>();
+		//configOverrides.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
+		//factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, configOverrides);
+		
+    	//factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
     	DatabaseInitializer.generateUsers(factory);
     	DatabaseInitializer.generateCourses(factory);
     	
