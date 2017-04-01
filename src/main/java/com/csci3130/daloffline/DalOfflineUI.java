5f3e8756c1ec60e3a6908a5aceeacb716bb027da
Connor Foran <connor@4n.ca>
2017-04-01 14:16:15 -0300

    jdbc?

--- src/main/java/com/csci3130/daloffline/DalOfflineUI.java
+++ src/main/java/com/csci3130/daloffline/DalOfflineUI.java
@@ -105,7 +105,8 @@ public class DalOfflineUI extends UI {
     		//factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
     	
     	Map<String, Object> configOverrides = new HashMap<String, Object>();
-		configOverrides.put("hibernate.connection.url", System.getenv("DATABASE_URL"));
+    	String env = System.getenv("JDBC_DATABASE_URL");
+		configOverrides.put("hibernate.connection.url", env);
 		factory = Persistence.createEntityManagerFactory("postgres", configOverrides);
 		
     	//factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
