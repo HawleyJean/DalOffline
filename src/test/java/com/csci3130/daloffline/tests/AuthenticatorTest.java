30d8d4e2d2e909a9624c2672b03d524b6df7ad5a
Andrew Muise <amuise3@hotmail.ca>
2017-04-05 17:57:59 -0300

    I don't think this will work on travis...

--- src/test/java/com/csci3130/daloffline/tests/AuthenticatorTest.java
+++ src/test/java/com/csci3130/daloffline/tests/AuthenticatorTest.java
@@ -2,6 +2,9 @@ package com.csci3130.daloffline.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -15,8 +18,19 @@ public class AuthenticatorTest {
 	@Test
 	public void authenticateTest() {
 
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("daloffline_db");
-
+//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("daloffline_db");
+		EntityManagerFactory factory;
+    	String env = System.getenv("JDBC_DATABASE_URL");
+    	
+    	if(env != null)
+    	{
+    		Map<String, Object> configOverrides = new HashMap<String, Object>();
+    		configOverrides.put("hibernate.connection.url", env);
+    		factory = Persistence.createEntityManagerFactory("postgres", configOverrides);
+    	}
+    	else
+    		factory = Persistence.createEntityManagerFactory("postgres");
+		
 		//this will have to be changed later when we are no longer "generating users" but creating users
 	    DatabaseInitializer.generateUsers(factory);
 		assertEquals(true, Authenticator.authenticate("student", "pass", factory));
