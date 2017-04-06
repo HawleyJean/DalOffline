166bc11e3d2f9a36623d4ae9d29073a298cb226f
Connor Foran <connor@4n.ca>
2017-04-06 13:29:44 -0300

    Seeing if database can start in travis

--- src/main/java/com/csci3130/daloffline/DalOfflineUI.java
+++ src/main/java/com/csci3130/daloffline/DalOfflineUI.java
@@ -68,17 +68,6 @@ public class DalOfflineUI extends UI {
     private User user;
     
     
-    
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
-    }
-    
 	/**
 	 * This function changes the view based on a VaadinRequest sent by some action
 	 * 
